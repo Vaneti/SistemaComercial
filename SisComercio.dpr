79fc1b855a6074e0fb21574eeeb53d5ee127ba0d
AlexsanderManzoli <alexsandermanzoli@gmail.com>
2015-09-23 22:08:58 -0300

    Criado data modulo e salvo na sua pasta devida, adicionado componentes para conexao no banco de dados e com isso alterado o nome do data modulo para dm e a tela principal para FRM_Principal e com isso o caption para Sistema comercial

--- SisComercio.dpr
+++ SisComercio.dpr
@@ -2,7 +2,8 @@ program SisComercio;
 
 uses
   Vcl.Forms,
-  UFRM_Principal in 'Formularios\UFRM_Principal.pas' {Form1};
+  UFRM_Principal in 'Formularios\UFRM_Principal.pas' {Form1},
+  UDM_Principal in 'DataModulos\UDM_Principal.pas' {DataModule1: TDataModule};
 
 {$R *.res}
 
@@ -10,5 +11,6 @@ begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
   Application.CreateForm(TForm1, Form1);
+  Application.CreateForm(TDataModule1, DataModule1);
   Application.Run;
 end.
