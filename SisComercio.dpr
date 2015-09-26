adffc1a47ec863b42808dbac424d63f7bd613abd
AlexsanderManzoli <alexsandermanzoli@gmail.com>
2015-09-26 11:45:39 -0300

    Configuração que foi feita no bramc desenvolvimento

--- SisComercio.dpr
+++ SisComercio.dpr
@@ -3,7 +3,7 @@ program SisComercio;
 uses
   Vcl.Forms,
   UFRM_Principal in 'Formularios\UFRM_Principal.pas' {Form1},
-  UDM_Principal in 'DataModulos\UDM_Principal.pas' {DataModule1: TDataModule},
+  UDM_Principal in 'DataModulos\UDM_Principal.pas' {Dm: TDataModule},
   UFRM_Modelo in 'Formularios\01-Cadastros\UFRM_Modelo.pas' {FRM_Modelo};
 
 {$R *.res}
@@ -12,7 +12,7 @@ begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
   Application.CreateForm(TForm1, Form1);
-  Application.CreateForm(TDataModule1, DataModule1);
+  Application.CreateForm(TDm, Dm);
   Application.CreateForm(TFRM_Modelo, FRM_Modelo);
   Application.Run;
 end.
